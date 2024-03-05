b5ed259827e18af1bf56d8085b3d06e2bedd69be
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-05 10:17:57 -0300

    feat: add competence model

---
+++ src/models/competence.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model, Document } from 'mongoose';
+
+import { UUID } from "crypto";
+import { title } from 'process';
+
+interface Competence extends Document{
+  id: UUID
+  title: `${string & {length: 100}}`;
+  description: `${string & {length: 255}}`;
+}
+
+const competenceSchema = new Schema<Competence>({
+  id: Schema.Types.UUID,
+  title: String,
+  description: String,
+});
+
+export default model<Competence>('Competence', competenceSchema);
+
