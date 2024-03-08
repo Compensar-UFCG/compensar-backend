9b714a6495cfed6d1f49111db324851025648157
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-08 19:30:39 -0300

    chore: solve lint problem

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -9,6 +9,7 @@ interface Error {
   message: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getErrorObject = (err: any): Error => {
   return {
     status: err?.status || err?.statusCode || 500,
