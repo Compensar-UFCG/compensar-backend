7fe7a350477516f38fef7eb1575705f9e7702c9a
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-08 18:49:58 -0300

    fix: add a new fixed port to run server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use('/api', competenceRoutes);
