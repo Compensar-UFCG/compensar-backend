5e7090a106913a0f026597bcb463b14429d7887d
Sheilla da Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-18 20:50:54 -0300

    feat: dd CRUD of questions (#7)
    * feat: add question models and interface
    
    * feat: add question validation and sanitization
    
    * feat: add question routes
    
    * test: add question unit test

---
+++ src/models/question.model.ts
@@ -0,0 +1,64 @@
+import { Schema, model } from 'mongoose';
+
+import { UUID } from 'crypto';
+
+type Font = "enem" | "pisa" | "olimpiadas" | "school" | "other"
+type Alternatives = string[];
+
+interface Question {
+  id: UUID;
+  title: string;
+  statement: string;
+  image?: string;
+  type: string;
+  font: Font;
+  year?: number;
+  alternatives?: Alternatives;
+  response: string;
+}
+
+const questionSchema = new Schema<Question>({
+  id: Schema.Types.UUID,
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  statement: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: false,
+    unique: false
+  },
+  type: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  font: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  year: {
+    type: Number,
+    required: false,
+    unique: false
+  },
+  alternatives: {
+    type: Schema.Types.Array,
+    required: false,
+    unique: false
+  },
+  response: {
+    type: String,
+    required: true,
+    unique: false
+  },
+});
+
+export default model<Question>('Question', questionSchema);
---
+++ src/routes/question.routes.ts
@@ -0,0 +1,112 @@
+import { Router, Request, Response } from 'express';
+import Question from '../models/question.model';
+import { getErrorObject } from '../utils/error';
+import { checkIsValidQuestionBody, questionValidationSchema, sanitizationQuestionBody } from '../utils/questions.validation';
+
+const router: Router = Router();
+
+router.get('/questions', async (_: Request, res: Response) => {
+  try {
+    const questions = await Question.find();
+    res.status(200).json(questions);
+  } catch (err) {
+    const error = getErrorObject(err);
+    res.status(error.status).json(error);
+  }
+});
+
+router.get('/questions/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const question = await Question.findById(id);
+    if (!question) {
+      return res.status(404).json({ message: 'Question not found' });
+    }
+    res.status(200).json(question);
+  } catch (err) {
+    const error = getErrorObject(err);
+    res.status(error.status).json(error);
+  }
+});
+
+router.post('/questions', sanitizationQuestionBody, questionValidationSchema, async (req: Request, res: Response) => {
+  const { isError, message } = checkIsValidQuestionBody(req);
+
+  if(isError) return res.status(422).json({ message })
+
+  const { title, statement, image, font, year, type, alternatives, response } = req.body;
+
+  try {
+    const question = new Question({
+      title,
+      statement,
+      image,
+      font,
+      year,
+      type,
+      alternatives,
+      response
+    });
+
+    const newQuestion = await question.save();
+    res.status(201).json({ message: `Created '${newQuestion.title}' with success`});
+  } catch (err) {
+    const error = getErrorObject(err);
+    res.status(error.status).json(error);
+  }
+});
+
+router.put('/questions/:id', sanitizationQuestionBody, questionValidationSchema, async (req: Request, res: Response) => {
+  const { isError, message } = checkIsValidQuestionBody(req);
+
+  if(isError) return res.status(422).json({ message })
+
+  const id = req.params.id;
+  const {
+    title,
+    statement,
+    image,
+    font,
+    year,
+    type,
+    alternatives,
+    response
+  } = req.body;
+
+  try {
+    const question = await Question.findByIdAndUpdate(id, {
+      title,
+      statement,
+      image,
+      font,
+      year,
+      type,
+      alternatives,
+      response
+    }, { new: true });
+    if (!question) {
+      return res.status(404).json({ message: 'Question not found' });
+    }
+    res.json({ message: `Updated '${question.title}' with success`});
+  } catch (err) {
+    const error = getErrorObject(err);
+    res.status(error.status).json(error);
+  }
+});
+
+router.delete('/questions/:id', async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const question = await Question.findByIdAndDelete(id);
+    if (!question) {
+      return res.status(404).json({ message: 'Question not found' });
+    }
+    res.status(200).json({ message: `Delete question '${question.title}' with success`});
+  } catch (err) {
+    const error = getErrorObject(err);
+    res.status(error.status).json(error);
+  }
+});
+
+export default router;
