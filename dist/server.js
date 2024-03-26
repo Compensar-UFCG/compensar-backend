7b6a4f86710ea75984007b9617a4b09d18a52014
Sheilla Silva <sheilla.silva@ccc.ufcg.edu.br>
2024-03-26 15:07:30 -0300

    chore: build project

--- dist/server.js
+++ dist/server.js
@@ -12,15 +12,16 @@ const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const login_routes_1 = __importDefault(require("./routes/login.routes"));
 const competenceQuestion_routes_1 = __importDefault(require("./routes/competenceQuestion.routes"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const authenticator_1 = __importDefault(require("./middlewares/authenticator"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
 app.use(body_parser_1.default.json());
-app.use('/api', competence_routes_1.default);
-app.use('/api', question_routes_1.default);
-app.use('/api', user_routes_1.default);
 app.use('/api', login_routes_1.default);
-app.use('/api', competenceQuestion_routes_1.default);
+app.use('/api', competence_routes_1.default);
+app.use('/api', user_routes_1.default);
+app.use('/api', authenticator_1.default, question_routes_1.default);
+app.use('/api', authenticator_1.default, competenceQuestion_routes_1.default);
 mongoose_1.default.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.beoiebt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => {
     console.log('Connected to MongoDB');
